07c6ea3085943bacab7f48f5efc45943247a1a50
Ahmad Mustafa <ahmadomar65@gmail.com>
2019-06-07 16:31:41 +0300

    Add files via upload

---
+++ Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  AirFix
+//
+//  Created by Ahmad Mustafa on 5/20/19.
+//  Copyright © 2019 Pixel. All rights reserved.
+//
+
+import Foundation
+
+class Message{
+    var sender: String = ""
+    var messageBody: String = ""
+}
---
+++ Model/menu.swift
@@ -0,0 +1,19 @@
+//
+//  menu.swift
+//  AirFix
+//
+//  Created by Ahmad Mustafa on 5/28/19.
+//  Copyright © 2019 Pixel. All rights reserved.
+//
+
+import Foundation
+
+class menu{
+    private(set) public var title: String
+    private(set) public var image: String
+    
+    init(title: String, image: String) {
+        self.title = title
+        self.image = image
+    }
+}
